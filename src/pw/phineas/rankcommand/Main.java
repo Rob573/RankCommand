e3212f9863df7e9ffabbc8834dd46773ec133447
Phineas <phineas@phinstech.com>
2016-01-31 00:56:33 +0000

    Woops, forgot the perm for reload

--- src/pw/phineas/rankcommand/Main.java
+++ src/pw/phineas/rankcommand/Main.java
@@ -1,8 +1,10 @@
 package pw.phineas.rankcommand;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import pw.phineas.rankcommand.commands.Rank;
-
 import java.io.IOException;
 
 /**
@@ -11,6 +13,7 @@ import java.io.IOException;
 public class Main extends JavaPlugin {
 
     public void onEnable() {
+
         try {
             Metrics metrics = new Metrics(this);
             metrics.start();
@@ -21,6 +24,25 @@ public class Main extends JavaPlugin {
         getCommand("rank").setExecutor(new Rank(this));
 
         saveDefaultConfig();
+    }
+
+    private String colorize(String input) {
+        return ChatColor.translateAlternateColorCodes('&', input);
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("rankreload")) {
+            if(sender.hasPermission("rankcommand.reload")) {
+                try {
+                    reloadConfig();
+                } finally {
+                    sender.sendMessage(ChatColor.GREEN + "Successfully reloaded config file from disk!");
+                }
+            } else {
+                sender.sendMessage(colorize(getConfig().getString("NoPermission")));
+            }
+        }
+        return true;
     }
 
     public void onDisable() {
