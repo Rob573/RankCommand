70b882f80d75c72fc38eb0526993e463f9cfa77f
Phineas <phineas@phinstech.com>
2016-03-10 19:41:38 +0000

    Make sure player isn't null before executing

--- src/pw/phineas/rankcommand/commands/Rank.java
+++ src/pw/phineas/rankcommand/commands/Rank.java
@@ -1,11 +1,11 @@
 package pw.phineas.rankcommand.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import pw.phineas.rankcommand.Main;
 import ru.tehkode.permissions.PermissionGroup;
@@ -33,22 +33,32 @@ public class Rank implements CommandExecutor {
             if(sender.hasPermission("rankcommand.use")) {
                 if(args.length > 2 || args.length < 2) {
                     sender.sendMessage(prefix + colorize(this.plugin.getConfig().getString("WrongArgs")));
-                    ((Player) sender).playSound(((Entity) sender).getLocation(), Sound.NOTE_BASS, 10, 1);
+                    ((Player) sender).playSound(((Player) sender).getLocation(), Sound.NOTE_BASS, 10, 1);
                 }
 
                 if(args.length == 2) {
-                    Player tplayer = sender.getServer().getPlayer(args[0]);
+                    String tplayer = null;
+
+                    try {
+                        tplayer = Bukkit.getPlayer(args[0]).getName();
+                    } catch (Exception e) {
+                        sender.sendMessage(prefix + ChatColor.GRAY + "That player is not online!");
+                        return false;
+                    }
+
+                    Player target = Bukkit.getPlayer(tplayer);
+
                     String group = args[1];
                     PermissionGroup[] groups = { PermissionsEx.getPermissionManager().getGroup(group) };
-                    PermissionsEx.getUser(tplayer).setGroups(groups);
-                    sender.sendMessage(prefix + colorize(this.plugin.getConfig().getString("SetRank")).replaceAll("%player%", tplayer.getName()).replaceAll("%rank%", group));
-                    tplayer.playSound(tplayer.getLocation(), Sound.NOTE_PIANO, 10, 1);
-                    ((Player) sender).playSound(((Entity) sender).getLocation(), Sound.NOTE_PIANO, 10, 1);
-                    tplayer.sendMessage(prefix + colorize(this.plugin.getConfig().getString("YourRankWasSet")).replaceAll("%rank%", group));
+                    PermissionsEx.getUser(target).setGroups(groups);
+                    sender.sendMessage(prefix + colorize(this.plugin.getConfig().getString("SetRank")).replaceAll("%player%", target.getName()).replaceAll("%rank%", group));
+                    target.playSound(target.getLocation(), Sound.NOTE_PIANO, 10, 1);
+                    ((Player) sender).playSound(((Player) sender).getLocation(), Sound.NOTE_PIANO, 10, 1);
+                    target.sendMessage(prefix + colorize(this.plugin.getConfig().getString("YourRankWasSet")).replaceAll("%rank%", group));
                 }
             } else {
                 sender.sendMessage(colorize(this.plugin.getConfig().getString("NoPermission")));
-                ((Player) sender).playSound(((Entity) sender).getLocation(), Sound.NOTE_BASS, 10, 1);
+                ((Player) sender).playSound(((Player) sender).getLocation(), Sound.NOTE_BASS, 10, 1);
             }
         }
         return false;
